fda79e4499144e34884f6a4556c2315c5fe49224
charlie keegan <chakeega@amazon.com>
2023-02-07 11:55:49 +0000

    initialse tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,11 @@ module "test_dbs" {
   source = "./modules/test_dbs"
 }
 
-
-module "test_dbs" {
+module "snapshots_tool_rds_dest" {
   source = "./modules/snapshots_tool_rds_dest"
+}
+
+
+module "snapshots_tool_rds_source" {
+  source = "./modules/snapshots_tool_rds_source"
 }
