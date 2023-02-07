fb6ca289f2d7c52dee386360119bc9c0aacec5a8
charlie keegan <chakeega@amazon.com>
2023-02-07 11:54:03 +0000

    fix typos

--- terraform/modules/snapshots_tool_rds_source/resource.tf
+++ terraform/modules/snapshots_tool_rds_source/resource.tf
@@ -18,10 +18,10 @@ resource "aws_sns_topic_policy" "snspolicy_snapshots_rds" {
   // ]
   policy = {
     Version = "2008-10-17"
-    Id = "__default_policy_ID"
+    Id      = "__default_policy_ID"
     Statement = [
       {
-        Sid = "__default_statement_ID"
+        Sid    = "__default_statement_ID"
         Effect = "Allow"
         Principal = {
           AWS = "*"
@@ -39,9 +39,9 @@ resource "aws_sns_topic_policy" "snspolicy_snapshots_rds" {
         ]
         Resource = "*"
         Condition = {
-          StringEquals = {
-            AWS:SourceOwner = data.aws_caller_identity.current.account_id
-          }
+          test     = "StringEquals"
+          variable = "AWS:SourceOwner"
+          values   = [data.aws_caller_identity.current.account_id]
         }
       }
     ]
@@ -69,7 +69,7 @@ resource "aws_cloudwatch_composite_alarm" "alarmcw_backups_failed" {
 }
 
 resource "aws_cloudwatch_composite_alarm" "alarmcw_share_failed" {
-  count = locals.Share ? 1 : 0
+  count           = locals.Share ? 1 : 0
   actions_enabled = "true"
   // CF Property(ComparisonOperator) = "GreaterThanOrEqualToThreshold"
   // CF Property(EvaluationPeriods) = "2"
@@ -90,7 +90,7 @@ resource "aws_cloudwatch_composite_alarm" "alarmcw_share_failed" {
 }
 
 resource "aws_cloudwatch_composite_alarm" "alarmcw_delete_old_failed" {
-  count = locals.DeleteOld ? 1 : 0
+  count           = locals.DeleteOld ? 1 : 0
   actions_enabled = "true"
   // CF Property(ComparisonOperator) = "GreaterThanOrEqualToThreshold"
   // CF Property(EvaluationPeriods) = "2"
@@ -169,20 +169,20 @@ resource "aws_iam_role" "iamrole_snapshots_rds" {
 resource "aws_lambda_function" "lambda_take_snapshots_rds" {
   code_signing_config_arn = {
     S3Bucket = var.code_bucket
-    S3Key = "take_snapshots_rds.zip"
+    S3Key    = "take_snapshots_rds.zip"
   }
   memory_size = 512
   description = "This functions triggers snapshots creation for RDS instances. It checks for existing snapshots following the pattern and interval specified in the environment variables with the following format: <dbinstancename>-YYYY-MM-DD-HH-MM"
   environment {
     variables = {
-      INTERVAL = var.backup_interval
-      PATTERN = var.instance_name_pattern
-      LOG_LEVEL = var.log_level
+      INTERVAL        = var.backup_interval
+      PATTERN         = var.instance_name_pattern
+      LOG_LEVEL       = var.log_level
       REGION_OVERRIDE = var.source_region_override
-      TAGGEDINSTANCE = var.tagged_instance
+      TAGGEDINSTANCE  = var.tagged_instance
     }
   }
-  role = aws_iam_role.iamrole_snapshots_rds.arn
+  role    = aws_iam_role.iamrole_snapshots_rds.arn
   runtime = "python3.7"
   handler = "lambda_function.lambda_handler"
   timeout = 300
@@ -192,19 +192,19 @@ resource "aws_lambda_function" "lambda_share_snapshots_rds" {
   count = locals.Share ? 1 : 0
   code_signing_config_arn = {
     S3Bucket = var.code_bucket
-    S3Key = "share_snapshots_rds.zip"
+    S3Key    = "share_snapshots_rds.zip"
   }
   memory_size = 512
   description = "This function shares snapshots created by the take_snapshots_rds function with DEST_ACCOUNT specified in the environment variables. "
   environment {
     variables = {
-      DEST_ACCOUNT = var.destination_account
-      LOG_LEVEL = var.log_level
-      PATTERN = var.instance_name_pattern
+      DEST_ACCOUNT    = var.destination_account
+      LOG_LEVEL       = var.log_level
+      PATTERN         = var.instance_name_pattern
       REGION_OVERRIDE = var.source_region_override
     }
   }
-  role = aws_iam_role.iamrole_snapshots_rds.arn
+  role    = aws_iam_role.iamrole_snapshots_rds.arn
   runtime = "python3.7"
   handler = "lambda_function.lambda_handler"
   timeout = 300
@@ -214,19 +214,19 @@ resource "aws_lambda_function" "lambda_delete_old_snapshots_rds" {
   count = locals.DeleteOld ? 1 : 0
   code_signing_config_arn = {
     S3Bucket = var.code_bucket
-    S3Key = "delete_old_snapshots_rds.zip"
+    S3Key    = "delete_old_snapshots_rds.zip"
   }
   memory_size = 512
   description = "This function deletes snapshots created by the take_snapshots_rds function. "
   environment {
     variables = {
-      RETENTION_DAYS = var.retention_days
-      PATTERN = var.instance_name_pattern
-      LOG_LEVEL = var.log_level
+      RETENTION_DAYS  = var.retention_days
+      PATTERN         = var.instance_name_pattern
+      LOG_LEVEL       = var.log_level
       REGION_OVERRIDE = var.source_region_override
     }
   }
-  role = aws_iam_role.iamrole_snapshots_rds.arn
+  role    = aws_iam_role.iamrole_snapshots_rds.arn
   runtime = "python3.7"
   handler = "lambda_function.lambda_handler"
   timeout = 300
--- terraform/modules/snapshots_tool_rds_source/variable.tf
+++ terraform/modules/snapshots_tool_rds_source/variable.tf
@@ -1,76 +1,76 @@
 variable "code_bucket" {
   description = "Name of the bucket that contains the lambda functions to deploy."
-  type = string
+  type        = string
 }
 
 variable "instance_name_pattern" {
-  description = "Python regex for matching cluster identifiers to backup. Use "ALL_INSTANCES" to back up every RDS instance in the region."
-  type = string
-  default = "ALL_INSTANCES"
+  description = "Python regex for matching cluster identifiers to backup. Use 'ALL_INSTANCES' to back up every RDS instance in the region."
+  type        = string
+  default     = "ALL_INSTANCES"
 }
 
 variable "backup_interval" {
   description = "Interval for backups in hours. Default is 24"
-  type = string
-  default = "24"
+  type        = string
+  default     = "24"
 }
 
 variable "destination_account" {
   description = "Destination account with no dashes."
-  type = string
-  default = "000000000000"
+  type        = string
+  default     = "000000000000"
 }
 
 variable "share_snapshots" {
-  type = string
+  type    = string
   default = "TRUE"
 }
 
 variable "backup_schedule" {
   description = "Backup schedule in Cloudwatch Event cron format. Needs to run at least once for every Interval. The default value runs once every at 1AM UTC. More information: http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html"
-  type = string
-  default = "0 1 * * ? *"
+  type        = string
+  default     = "0 1 * * ? *"
 }
 
 variable "retention_days" {
   description = "Number of days to keep snapshots in retention before deleting them"
-  type = string
-  default = "7"
+  type        = string
+  default     = "7"
 }
 
 variable "log_level" {
   description = "Log level for Lambda functions (DEBUG, INFO, WARN, ERROR, CRITICAL are valid values)."
-  type = string
-  default = "ERROR"
+  type        = string
+  default     = "ERROR"
 }
 
 variable "lambda_cw_log_retention" {
   description = "Number of days to retain logs from the lambda functions in CloudWatch Logs"
-  type = string
-  default = "7"
+  type        = string
+  default     = "7"
 }
 
 variable "source_region_override" {
   description = "Set to the region where your RDS instances run, only if such region does not support Step Functions. Leave as NO otherwise"
-  type = string
-  default = "NO"
+  type        = string
+  default     = "NO"
 }
 
 variable "delete_old_snapshots" {
   description = "Set to TRUE to enable deletion of snapshot based on RetentionDays. Set to FALSE to disable"
-  type = string
-  default = "TRUE"
+  type        = string
+  default     = "TRUE"
 }
 
 variable "tagged_instance" {
   description = "Set to TRUE to filter instances that have tag CopyDBSnapshot set to True. Set to FALSE to disable"
-  type = string
-  default = "FALSE"
+  type        = string
+  default     = "FALSE"
 }
 
 variable "log_group_name" {
   description = "Name for RDS snapshot log group."
-  type = string
-  default = "lambdaDeleteOldSnapshotsRDS-source"
+  type        = string
+  default     = "lambdaDeleteOldSnapshotsRDS-source"
 }
 
